2a4f97cf3de3c719b297e7d69c32f03665e701a3
Justin Ball <justinball@gmail.com>
2022-04-19 14:33:00 -0600

    Initial attempt to bring over code

---
+++ lib/atomic_lti/exceptions.rb
@@ -0,0 +1,24 @@
+module AtomicLti
+  module Exceptions
+    class LineItemError < StandardError
+    end
+
+    class ConfigurationError < StandardError
+    end
+
+    class NamesAndRolesError < StandardError
+    end
+
+    class ScoreError < StandardError
+    end
+
+    class NoLTIDeployment < StandardError
+    end
+
+    class InvalidIssuer < StandardError
+    end
+
+    class InvalidOIDCRegistrationEndpoint < StandardError
+    end
+  end
+end
---
+++ lib/atomic_lti/services/results.rb
@@ -0,0 +1,18 @@
+module AtomicLti
+  module Services
+    # Canvas API docs: https://canvas.instructure.com/doc/api/result.html
+    class Results < AtomicLti::Services::Base
+
+      def list(line_item_id)
+        url = "#{line_item_id}/results"
+        HTTParty.get(url, headers: headers)
+      end
+
+      def show(line_item_id, result_id)
+        url = "#{line_item_id}/results/#{result_id}"
+        HTTParty.get(url, headers: headers)
+      end
+
+    end
+  end
+end
