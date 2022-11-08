bea293f87d6b26c2c312704ecc3541730ea11bce
Nick Benoit <nick.benoit@atomicjolt.com>
2022-11-08 07:30:32 -0700

    Allow initializing service with iss / deployment id

--- app/lib/atomic_lti/services/base.rb
+++ app/lib/atomic_lti/services/base.rb
@@ -2,12 +2,19 @@ module AtomicLti
   module Services
     class Base
 
-      def initialize(lti_token:)
-        iss = lti_token.dig('iss')
-        deployment_id = lti_token.dig(AtomicLti::Definitions::DEPLOYMENT_ID)
+      def initialize(lti_token: nil, iss: nil, deployment_id: nil)
+
+        token_iss = nil
+        token_deployment_id = nil
+
+        if lti_token.present?
+          token_iss = lti_token.dig('iss')
+          token_deployment_id = lti_token.dig(AtomicLti::Definitions::DEPLOYMENT_ID)
+        end
+
         @lti_token = lti_token
-        @iss = iss
-        @deployment_id = deployment_id
+        @iss = iss || token_iss
+        @deployment_id = deployment_id || token_deployment_id
       end
 
       def headers(options = {})
