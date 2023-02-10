c209ba10ef9bd829ad522c4a4de119c9fcc90a45
Justin Ball <justinball@gmail.com>
2023-02-10 10:42:30 -0700

    Changed LTI validation to list all errors

--- app/lib/atomic_lti/lti.rb
+++ app/lib/atomic_lti/lti.rb
@@ -5,48 +5,54 @@ module AtomicLti
         raise AtomicLti::Exceptions::InvalidLTIToken
       end
 
+      errors = []
+
       if decoded_token["iss"].blank?
-        raise AtomicLti::Exceptions::InvalidLTIToken.new("LTI token is missing required field iss")
+        errors.push("LTI token is missing required field iss")
       end
 
       if decoded_token["sub"].blank?
-        raise AtomicLti::Exceptions::InvalidLTIToken.new("LTI token is missing required field sub")
+        errors.push("LTI token is missing required field sub")
       end
 
       if decoded_token[AtomicLti::Definitions::DEPLOYMENT_ID].blank?
-        raise AtomicLti::Exceptions::InvalidLTIToken.new(
+        errors.push(
           "LTI token is missing required field #{AtomicLti::Definitions::DEPLOYMENT_ID}"
         )
       end
 
       if decoded_token[AtomicLti::Definitions::TARGET_LINK_URI_CLAIM].blank?
-        raise AtomicLti::Exceptions::InvalidLTIToken.new(
+        errors.push(
           "LTI token is missing required claim #{AtomicLti::Definitions::TARGET_LINK_URI_CLAIM}"
         )
       end
 
       if decoded_token[AtomicLti::Definitions::RESOURCE_LINK_CLAIM].blank?
-        raise AtomicLti::Exceptions::InvalidLTIToken.new(
+        errors.push(
           "LTI token is missing required claim #{AtomicLti::Definitions::RESOURCE_LINK_CLAIM}"
         )
       end
 
-      if decoded_token[AtomicLti::Definitions::RESOURCE_LINK_CLAIM]["id"].blank?
-        raise AtomicLti::Exceptions::InvalidLTIToken.new(
+      if decoded_token.dig(AtomicLti::Definitions::RESOURCE_LINK_CLAIM, "id").blank?
+        errors.push(
           "LTI token is missing required field id from the claim #{AtomicLti::Definitions::RESOURCE_LINK_CLAIM}"
         )
       end
 
       if decoded_token[AtomicLti::Definitions::MESSAGE_TYPE].blank?
-        raise AtomicLti::Exceptions::InvalidLTIToken.new(
+        errors.push(
           "LTI token is missing required claim #{AtomicLti::Definitions::MESSAGE_TYPE}"
         )
       end
 
       if decoded_token[AtomicLti::Definitions::ROLES_CLAIM].blank?
-        raise AtomicLti::Exceptions::InvalidLTIToken.new(
+        errors.push(
           "LTI token is missing required claim #{AtomicLti::Definitions::ROLES_CLAIM}"
         )
+      end
+
+      if errors.length > 0
+        raise AtomicLti::Exceptions::InvalidLTIToken.new(errors.join(" "))
       end
 
       if decoded_token[AtomicLti::Definitions::LTI_VERSION].blank?
