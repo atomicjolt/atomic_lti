34959a24dcc1150ea3bb48cae8fdc4590c88fff6
Matt Petro <matt.petro@atomicjolt.com>
2023-04-26 10:17:11 -0400

    Remove broken asset config

--- lib/atomic_lti/engine.rb
+++ lib/atomic_lti/engine.rb
@@ -5,9 +5,5 @@ module AtomicLti
     config.generators do |g|
       g.test_framework :rspec
     end
-
-    initializer "atomic_lti.assets.precompile" do |app|
-      app.config.assets.precompile += %w(init.js redirect.js application.css jwks.css launch.css)
-    end
   end
 end
