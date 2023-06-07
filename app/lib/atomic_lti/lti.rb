76276deacc0c1be3e6a2885d3f8162e3d117ed4f
sean.collings@atomicjolt.com <sean.collings@atomicjolt.com>
2023-06-07 08:26:53 -0600

    remove rubocop warnings

--- app/lib/atomic_lti/lti.rb
+++ app/lib/atomic_lti/lti.rb
@@ -1,6 +1,15 @@
 module AtomicLti
   class Lti
 
+    REQUIRED_CLAIMS = [
+      "iss",
+      "aud",
+      "exp",
+      "iat",
+      AtomicLti::Definitions::DEPLOYMENT_ID,
+      AtomicLti::Definitions::MESSAGE_TYPE,
+    ].freeze
+
     def self.validate!(decoded_token, requested_target_link_uri = nil, validate_target_link_url = false)
       if decoded_token.blank?
         raise AtomicLti::Exceptions::InvalidLTIToken
@@ -8,58 +17,19 @@ module AtomicLti
 
       errors = []
 
-      if decoded_token["iss"].blank?
-        errors.push("LTI token is missing required field iss")
-      end
+      errors.concat(validate_required_claims(decoded_token))
+      errors.concat(validate_aud_claim(decoded_token))
+      errors.concat(validate_roles_claim(decoded_token))
 
       if decoded_token["sub"].blank? && !AtomicLti.allow_anonymous_user
         errors.push("LTI token is missing required field sub")
-      end
-
-      if decoded_token["aud"].blank?
-        errors.push("LTI token is missing required field aud")
-      end
-
-      if decoded_token["aud"].is_a?(Array) && decoded_token["aud"].length > 1
-        # OpenID Connect spec specifies the AZP should exist and be an AUD
-        if decoded_token["azp"].blank?
-          errors.push("LTI token has multiple aud and is missing required field azp")
-        elsif decoded_token["aud"].exclude?(decoded_token["azp"])
-          errors.push("LTI token azp is not one of the aud's")
-        end
-      end
-
-      if decoded_token[AtomicLti::Definitions::DEPLOYMENT_ID].blank?
-        errors.push(
-          "LTI token is missing required field #{AtomicLti::Definitions::DEPLOYMENT_ID}"
-        )
-      end
-
-      if decoded_token[AtomicLti::Definitions::MESSAGE_TYPE].blank?
-        errors.push(
-          "LTI token is missing required claim #{AtomicLti::Definitions::MESSAGE_TYPE}"
-        )
       end
 
       if decoded_token[AtomicLti::Definitions::MESSAGE_TYPE] === "LtiResourceLinkRequest"
         errors.concat(validate_resource_link_request(decoded_token, requested_target_link_uri, validate_target_link_url))
       end
 
-      if decoded_token[AtomicLti::Definitions::ROLES_CLAIM].nil?
-        errors.push(
-          "LTI token is missing required claim #{AtomicLti::Definitions::ROLES_CLAIM}"
-        )
-      end
-
-      roles = decoded_token[AtomicLti::Definitions::ROLES_CLAIM]
-      if roles.is_a?(Array) && !roles.empty?
-        invalid_roles = roles - AtomicLti::Definitions::ROLES
-        if invalid_roles.length == roles.length
-          errors.push("LTI token has invalid roles: #{invalid_roles.join(", ")}")
-        end
-      end
-
-      if errors.length > 0
+      if !errors.empty?
         raise AtomicLti::Exceptions::InvalidLTIToken.new(errors.join(" "))
       end
 
@@ -70,6 +40,53 @@ module AtomicLti
       raise AtomicLti::Exceptions::InvalidLTIVersion unless valid_version?(decoded_token)
 
       true
+    end
+
+    def self.validate_required_claims(decoded_token)
+      errors = []
+
+      REQUIRED_CLAIMS.each do |claim|
+        if decoded_token[claim].blank?
+          errors.push("LTI token is missing required claim #{claim}")
+        end
+      end
+
+      errors
+    end
+
+    def self.validate_aud_claim(decoded_token)
+      errors = []
+
+      if decoded_token["aud"].is_a?(Array) && decoded_token["aud"].length > 1
+        # OpenID Connect spec specifies the AZP should exist and be an AUD
+        if decoded_token["azp"].blank?
+          errors.push("LTI token has multiple aud and is missing required field azp")
+        elsif decoded_token["aud"].exclude?(decoded_token["azp"])
+          errors.push("LTI token azp is not one of the aud's")
+        end
+      end
+
+      errors
+    end
+
+    def self.validate_roles_claim(decoded_token)
+      errors = []
+
+      if decoded_token[AtomicLti::Definitions::ROLES_CLAIM].nil?
+        errors.push(
+          "LTI token is missing required claim #{AtomicLti::Definitions::ROLES_CLAIM}",
+        )
+      end
+
+      roles = decoded_token[AtomicLti::Definitions::ROLES_CLAIM]
+      if roles.is_a?(Array) && !roles.empty?
+        invalid_roles = roles - AtomicLti::Definitions::ROLES
+        if invalid_roles.length == roles.length
+          errors.push("LTI token has invalid roles: #{invalid_roles.join(', ')}")
+        end
+      end
+
+      errors
     end
 
     def self.validate_resource_link_request(decoded_token, requested_target_link_uri = nil, validate_target_link_url = false)
