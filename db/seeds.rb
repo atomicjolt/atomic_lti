ff9f1c2fba3462b40a60456acff6d557c1c9457e
Matt Petro <matt.petro@atomicjolt.com>
2023-07-24 10:30:39 -0400

    Update Instructure OIDC urls
    https://community.canvaslms.com/t5/The-Product-Blog/Minor-LTI-1-3-Changes-New-OIDC-Auth-Endpoint-Support-for/ba-p/551677

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,16 +2,16 @@
 AtomicLti::Jwk.find_or_create_by(domain: nil)
 
 # Add some platforms
-AtomicLti::Platform.create_with( 
-  jwks_url: "https://canvas.instructure.com/api/lti/security/jwks", 
-  token_url: "https://canvas.instructure.com/login/oauth2/token", 
-  oidc_url: "https://canvas.instructure.com/api/lti/authorize_redirect"
+AtomicLti::Platform.create_with(
+  jwks_url: "https://sso.canvaslms.com/api/lti/security/jwks",
+  token_url: "https://sso.canvaslms.com/login/oauth2/token",
+  oidc_url: "https://sso.canvaslms.com/api/lti/authorize_redirect"
 ).find_or_create_by(iss: "https://canvas.instructure.com")
 
 AtomicLti::Platform.create_with(
-  jwks_url: "https://canvas-beta.instructure.com/api/lti/security/jwks",
-  token_url: "https://canvas-beta.instructure.com/login/oauth2/token",
-  oidc_url: "https://canvas-beta.instructure.com/api/lti/authorize_redirect",
+  jwks_url: "https://sso.beta.canvaslms.com/api/lti/security/jwks",
+  token_url: "https://sso.beta.canvaslms.com/login/oauth2/token",
+  oidc_url: "https://sso.beta.canvaslms.com/api/lti/authorize_redirect",
 ).find_or_create_by(iss: "https://canvas-beta.instructure.com")
 
 
