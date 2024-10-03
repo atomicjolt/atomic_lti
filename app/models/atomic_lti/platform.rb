6f4b43e425e47c889152ab03353ca75ea7559af0
David Spencer <david.spencer@atomicjolt.com>
2024-10-03 13:22:29 -0400

    Add platform query_constraints
    We use iss as the primary key for Platform

--- app/models/atomic_lti/platform.rb
+++ app/models/atomic_lti/platform.rb
@@ -1,5 +1,6 @@
 module AtomicLti
   class Platform < ApplicationRecord
+    query_constraints :iss
     validates :iss, presence: true
     has_many :platform_instances, foreign_key: :iss, primary_key: :iss
     has_many :deployments, foreign_key: :iss, primary_key: :iss
